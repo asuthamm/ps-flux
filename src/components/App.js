796c9329470fb8a6b67bb2065c89e8ea2a9097f0
asuthamm <asuthamm@gmail.com>
2020-04-14 10:32:23 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+
+
+function App() {
+
+  const route = window.location.pathname
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
